43f5bb38d342c2412219a2b1a5f256b90f68874d
Sereina Blanchard <wintermist3@gmail.com>
2017-11-21 06:53:27 -0800

    added styling

--- src/js/App.js
+++ src/js/App.js
@@ -12,16 +12,14 @@ class App extends React.Component {
     this.state = {
       zipcode: "",
       forecast: [],
+      background: "",
+      location: "",
       api: ApiKey
     }
   }
 
-  // componentDidMount() {
-  //   this.fetchData();
-  // }
-
   fetchData() {
-    const {api, zipcode} = this.state;
+    const {api, zipcode, background, location} = this.state;
     
     fetch("http://api.openweathermap.org/data/2.5/forecast?zip="+zipcode+",us&cnt=8&units=imperial&APPID="+api)
       .then(response => response.json())
@@ -32,7 +30,7 @@ class App extends React.Component {
           weatherDescription: `${data.weather[0].description}`,
           temp: Math.round(`${data.main.temp}`),
       })))
-      .then(forecast => this.setState ({forecast}))
+      .then(forecast => this.setState ({forecast, background: forecast[0].weather, location: forecast[0].city}))
       .catch(error => console.log("Parsing failed: " + error))
   }
 
@@ -45,22 +43,63 @@ class App extends React.Component {
 
     // update state with new zipcode
     this.setState({zipcode: newZip})
+
+    // if(zipcode.length === 5) {
+    //   this.fetchData();
+    // }
   }
 
 
+  clothingAdvice() {
+
+    if(weather <= 32) {
+      ["Burrrr! It be chilly out there. Remember your winter parka", "It's crazy cold out there. Don't forget your hat, scarf, and gloves. You don't wanna turn into a human popsicle out there!"]
+    }
+
+    if(weather > 32 && weather < 45) {
+      ["A fuzzy hat and scarf would probably be nice", "Time to bring out the winter coat!"]
+    }
+
+    if(weather >= 45 && weather < 65) {
+      ["A light jacked would be a good choice."]
+    }
+
+    if(weather >= 65 && weather < 85) {
+      ["It's pretty hot out there, remember to wear something light and airy.", ]
+    }
+
+    if(weather > 85) {
+      ["Dang, it be hot out there! You should head to the beach or something.", "It be crazy hot out there, remember to stay hydrated!"]
+    }
+  }
+
   render(){
     // Get zip code and forecast from state
-    const {zipcode, forecast} = this.state;
+    const {zipcode, forecast, location} = this.state;
 
     return (
-      <div>
-        <Navbar/>
-        <header>
-          <h1>Weather Wear</h1>
-        </header>
-        <div className="content">
-          <input type="text" placeholder="zip code" onChange={(e) => {this.onZipcodeChange(e);}}/>
-          <button className="btn btn-lg btn-success" onClick={(e) => {this.fetchData();}}>Get Weather</button>
+      <div className="main-container">
+        {/*<Navbar/>*/}
+        
+        <div className="white-container">
+          <div className="search-container">
+            <h1 className="zip-title">Enter a Zip Code!</h1>
+            <input type="text" className="zip-input form-control" placeholder="zip code" onChange={(e) => {this.onZipcodeChange(e);}}/>
+            <button className="zip-btn btn btn-md btn-success" onClick={(e) => {this.fetchData();}}>Get Weather</button>
+          </div>
+
+          <div className="results-container">
+          {
+            location ? <h1 className="city-title">{location}</h1> : null
+          }
+
+          {/*{
+            <div>
+              <h1>FIRST ELEMENT ONLY</h1>
+              <h2>{this.state.forecast[0]}</h2>
+            </div>
+          }*/}
+
           {
             forecast ? forecast.map(singleForecast => {
               const {date, city, weather, weatherDescription, temp, icon} = singleForecast;
@@ -68,16 +107,16 @@ class App extends React.Component {
               return(
                 <div key={date} className="weather-forecast">
                   {/*<h2>{weather}</h2>*/}
-                  <img src={require(`../images/${weather}.png`)} alt={`Weather Forecast: ${weatherDescription}`}/>
+                  <img src={require(`../images/${weather}.png`)} alt={`Weather Forecast: ${weatherDescription}`} className="weather-img"/>
                   <h2 className="weather-description">{weatherDescription}</h2>
-                  <p className="temp">{temp}&deg;F</p>
+                  <p className="temperature">{temp}&deg;F</p>
                   <Timestamp time={date} format='full' className="date"/>
                   <hr/>
                 </div>
               )
             }) : null
           }
-
+          </div>
         </div>
       </div>
     )
